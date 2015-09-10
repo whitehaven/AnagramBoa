dce0ccf1d13c069f2b91d33896a6e579dc93026f
whitehaven <accrist@gmail.com>
2015-09-10 16:12:44 -0600

    super-early prototype wordfinder function

--- Boa.py
+++ Boa.py
@@ -7,11 +7,11 @@ script, dictFileAddress = argv
 
 targetWord = "lormncae"  # subsitute for input() something
 
-visitedLetters = []
+visited_letters = []
 
 # create visitedLetters with as many list componenets as letters in targetWord
 for letters in targetWord:
-    visitedLetters.append(False)
+    visited_letters.append(False)
 
 # build dictionary set
 dictFile = open(dictFileAddress)
@@ -34,10 +34,12 @@ for dword in dictionary:
     for letter in range(0, len(dword) - 1):
         letterTrees[letter].add(dword[letter])
 
+partialAnagrams = []
+completeAnagrams = []
 
-# TODO main algorithm
-# for each starting letter
-#   is word(word, index, visitedLetters, wordsSoFar)
+
+# TODO write recursive isWord() function
+#   is word(word, index, visitedLetters, visited_letters, wordsSoFar)
 #       if letter in dictionary(char)
 #           if complete word
 #               write to fragment anagrams
@@ -48,3 +50,29 @@ for dword in dictionary:
 #                   return is word(word, index+1, visitedLetters, wordsSoFar)
 #       elif (letters aren't in dictionary)
 #           return false
+
+def isWord(word, index, word_so_far, visited_letters, words_so_far):
+    if word[index] in letterTrees[index]:
+        # add letter onto new word (must create new one because strings are immutable)
+        new_word_so_far = word_so_far + word[index]
+
+        # mark visited_letters
+        visited_letters[index] = True
+
+        if new_word_so_far in dictionary:
+            words_so_far.append(new_word_so_far)
+            partialAnagrams.append(new_word_so_far)
+
+            # if there's no False record in visited_letters, we're at the end
+            if not (False in visited_letters):
+                completeAnagrams.append(words_so_far)
+        for letter in visited_letters:
+            if letter == True:
+                isWord(word, index + 1, new_word_so_far, visited_letters, words_so_far)
+    return False
+
+
+# TODO main algorithm
+# for each starting letter
+for letter in range(0, len(targetWord) - 1):
+    isWord(targetWord, letter, "", visited_letters, [])
--- README.md
+++ README.md
@@ -3,8 +3,11 @@ Anagram finder/solver using recursion and cool tree data structures.
 
 One innovation here is that I'm making a set of possibilities for each word in a list indexed by letter position.
     Observe the difference:
-        Linear:
-            try every word from a set made from a dictionary at position ( O(n) for every one! ) ( O(n*m) total )
         
-        Tree:
-            find in a set[index] ( O(logn) for each one ) ( O(m*logn) total )
+    Linear:
+        try every word from a set made from a dictionary at position ( O(n) for every one! ) ( O(n*m) total )
+    
+    Tree:
+        find in a set[index] ( O(logn) for each one ) ( O(m*logn) total )
+            
+Anagrams will probably not be permuted in order unless I decide to do that later.
